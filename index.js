ce00bdaaa81171e3215cb0a1e8aef20e4fd7224f
Yuemin.Hu <hym920408@gmail.com>
2018-06-23 23:56:23 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const { app, BrowserWindow, ipcMain, Tray } = require("electron");
+const path = require("path");
+
+let tray = null;
+let window = null;
+
+const assetsDir = path.join(__dirname, "./assets");
+
+// app not in the dock
+// app.dock.
+app.dock.hide();
+
+app.on("ready", () => {
+  createTray();
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, "./assets/btc1.png"));
+  // tray.setTitle("hhehda");
+  tray.on("right-click", toggleWindow);
+  tray.on("double-click", toggleWindow);
+  tray.on("click", e => {
+    toggleWindow();
+    // console.log(process.defaultApp, e.metaKey)
+    window.openDevTools({
+      mode: "detach"
+    });
+    if (window.isVisible() && process.defaultApp && e.metaKey) {
+    }
+  });
+};
+
+
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 400,
+    // height: 400,
+    minHeight: 200,
+    maxHeight: 400,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+    webPreferences: {
+      // Prevents renderer process code from not running when window is
+      // hidden
+      backgroundThrottling: false
+    }
+  });
+  window.loadURL(`file://${path.join(__dirname, "index.html")}`);
+  // Hide the window when it loses focus
+  window.on("focus", () => {
+    window.webContents.send("event-window-focus");
+  });
+  window.on("blur", () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide();
+    }
+    window.webContents.send("event-window-blur");
+  });
+  window.focus();
+};
+
+const toggleWindow = () => {
+  if (window.isVisible()) {
+    window.hide();
+  } else {
+    showWindow();
+  }
+};
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.show();
+  window.focus();
+};
+
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+  // console.log(trayBounds)
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(
+    trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+  );
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4);
+  //
+  return { x: x, y: y };
+};
+
+ipcMain.on("show-window", () => {
+  showWindow();
+});
+
+ipcMain.on("heheE", (e, data) => console.log(data));
---
+++ page.js
@@ -0,0 +1,186 @@
+const { ipcRenderer } = require("electron");
+// const pako = require("pako");
+const fs = require("fs");
+// fs.readdir("./assets", (err, files) => console.log(files));
+// ipcRenderer.send('heheE', '3333')
+
+// const wsAddr = "wss://api.huobi.pro/ws";
+
+// function subscribe(ws) {
+//   const symbols = ["bchusdt"];
+//   // 订阅深度
+//   // 谨慎选择合并的深度，ws每次推送全量的深度数据，若未能及时处理容易引起消息堆积并且引发行情延时
+//   for (let symbol of symbols) {
+//     ws.send(
+//       JSON.stringify({
+//         sub: `market.${symbol}.depth.step0`,
+//         id: `${symbol}`
+//       })
+//     );
+//   }
+// }
+
+// function init() {
+//   const ws = new WebSocket(wsAddr);
+//   ws.addEventListener("open", () => {
+//     console.log("ws opened!");
+//     subscribe(ws);
+//   });
+//   ws.addEventListener("message", data => {
+//     const text = pako.deflate(data, {
+//       to: "string"
+//     });
+//     const msg = JSON.parse(text);
+//     if (msg.ping) {
+//       console.log("ping received!");
+//       ws.send(
+//         JSON.stringify({
+//           pong: msg.ping
+//         })
+//       );
+//     } else if (msg.tick) {
+//       console.log(msg);
+//     } else {
+//       console.log(msg);
+//     }
+//   });
+//   ws.addEventListener("close", () => {
+//     console.log("close");
+//     setTimeout(init, 1000);
+//   });
+//   ws.addEventListener("error", err => {
+//     console.log("error", err);
+//     setTimeout(init, 1000);
+//   });
+// }
+// init();
+(function() {
+  let symbols = ["btc"];
+
+  const mainWrapper = document.querySelector(".scrollable");
+  const outerWrapper = document.querySelector("div.mainWrapper");
+  const themeCheckBox = document.querySelector("input#themeChanger");
+  const themeSwitch = document.querySelector(".themeWrapper > span");
+  const lastFetchedTime = document.querySelector(
+    ".infoAndThettings > span:first-of-type > span"
+  );
+  const lastFetchImg = document.querySelector(
+    ".infoAndThettings > span:first-of-type > img"
+  );
+  const addSymbolButton = document.querySelector("div.add");
+
+  const ifDarkend = localStorage.getItem("themeDarken");
+  if (ifDarkend) {
+    outerWrapper.classList.add("darken");
+    themeCheckBox.checked = true;
+  }
+  // if is fetching data...
+  let fetching = false;
+
+  function fetchLatestPrice() {
+    let promiseArr = [];
+    for (let i of symbols) {
+      promiseArr.push(
+        fetch(`https://data.gateio.io/api2/1/ticker/${i}_usdt`).then(data =>
+          data.json()
+        )
+      );
+    }
+    // return new Promise((resolve, reject))
+    return Promise.all(promiseArr).then(data => {
+      // console.log(data);
+      lastFetchedTime.innerHTML = Date.now();
+      renderToPage(data);
+      return data;
+    });
+    // console.log(resps);
+  }
+
+  // mainWrapper.
+
+  function renderToPage(dataArr) {
+    for (let [index, val] of dataArr.entries()) {
+      const thisSymbol = symbols[index];
+      let thisSymbolWrapper = document.querySelector(
+        `div[data-symbol=${thisSymbol}]`
+      );
+      if (!thisSymbolWrapper) {
+        thisSymbolWrapper = document.createElement("div");
+        thisSymbolWrapper.setAttribute("data-symbol", thisSymbol);
+        thisSymbolWrapper.setAttribute("class", "symbol-block");
+        mainWrapper.appendChild(thisSymbolWrapper);
+      }
+      thisSymbolWrapper.innerHTML = `<div class='symbol-name'>${thisSymbol.toUpperCase()}-USDT</div><div class='lowHigh'><p>low:${Number(
+        val.low24hr
+      ).toFixed(2)}</p><p>high:${Number(val.high24hr).toFixed(
+        2
+      )}</p></div><div class='last'>${val.last}</div><div class=${
+        Number(val.percentChange) >= 0 ? "percentage raise" : "percentage"
+      }>${val.percentChange >= 0 ? "+" : ""}${Number(val.percentChange).toFixed(
+        2
+      )}%</div>`;
+    }
+  }
+
+  let fetchInterval = 10000;
+  let mainTimer = null;
+
+  fetchLatestPrice();
+  mainTimer = setTimeout(function inner() {
+    try {
+      fetchLatestPrice();
+      mainTimer = setTimeout(inner, fetchInterval);
+    } catch (e) {
+      console.log(e);
+      mainTimer = setTimeout(inner, fetchInterval);
+    }
+  }, fetchInterval);
+
+  ipcRenderer.on("event-window-blur", e => {
+    console.log("blured!");
+    fetchInterval = 60000;
+    // clearTimeout(mainTimer)
+  });
+
+  ipcRenderer.on("event-window-focus", e => {
+    console.log("focus!");
+    fetchInterval = 5000;
+  });
+
+  // change theme
+
+  themeSwitch.addEventListener("click", e => {
+    // const ifDarkend = Array.prototype.includes.call(
+    //   outerWrapper.classList,
+    //   "darken"
+    // );
+    const ifDarkend = themeCheckBox.checked;
+    if (!ifDarkend) {
+      localStorage.setItem("themeDarken", 1);
+    } else {
+      localStorage.removeItem("themeDarken");
+    }
+    themeCheckBox.checked = !ifDarkend;
+    // if (ifDarkend) {
+    outerWrapper.classList.toggle("darken");
+    // }
+  });
+
+  // fetch now!
+  lastFetchImg.addEventListener("click", e => {
+    if (fetching) {
+      return;
+    }
+    fetching = true;
+    e.target.classList.add("spinning");
+    fetchLatestPrice().then(data => {
+      fetching = false;
+      e.target.classList.remove("spinning");
+    });
+  });
+
+  // add symbol
+  addSymbolButton.addEventListener('click', e => {
+    
+  })
+})();
